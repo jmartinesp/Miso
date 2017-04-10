cdba3ec9de63d99d778d41c42a7be8df80396ceb
Jorge Martín <jorgemartinespinosa@gmail.com>
2017-04-10 08:48:37 +0200

    Initial Commit

---
+++ SwiftySoup/SwiftySoup.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySoup.h
+//  SwiftySoup
+//
+//  Created by Jorge Martín Espinosa on 10/4/17.
+//  Copyright © 2017 Jorge Martín Espinosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftySoup.
+FOUNDATION_EXPORT double SwiftySoupVersionNumber;
+
+//! Project version string for SwiftySoup.
+FOUNDATION_EXPORT const unsigned char SwiftySoupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySoup/PublicHeader.h>
+
+
---
+++ SwiftySoupTests/SwiftySoupTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftySoupTests.swift
+//  SwiftySoupTests
+//
+//  Created by Jorge Martín Espinosa on 10/4/17.
+//  Copyright © 2017 Jorge Martín Espinosa. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftySoup
+
+class SwiftySoupTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
