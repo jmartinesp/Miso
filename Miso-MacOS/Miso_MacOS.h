6dd34f14051741d39bf774bde03fa820f10e5e0d
Jorge Martin <jorge.martin@buddi.co.uk>
2019-11-04 19:29:30 +0100

    Try to add MacOS support

---
+++ Miso-MacOS/Miso_MacOS.h
@@ -0,0 +1,19 @@
+//
+//  Miso_MacOS.h
+//  Miso-MacOS
+//
+//  Created by Jorge Martín Espinosa on 04/11/2019.
+//  Copyright © 2019 Jorge Martín Espinosa. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for Miso_MacOS.
+FOUNDATION_EXPORT double Miso_MacOSVersionNumber;
+
+//! Project version string for Miso_MacOS.
+FOUNDATION_EXPORT const unsigned char Miso_MacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Miso_MacOS/PublicHeader.h>
+
+
